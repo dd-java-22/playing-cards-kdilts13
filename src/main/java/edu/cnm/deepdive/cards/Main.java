77bd7f5aef8061caeeb50dbc95813f61d68f3831
Kevin Dilts <kdilts@cnm.edu>
2026-02-02 10:02:36 -0700

    forgot to commit main

---
+++ src/main/java/edu/cnm/deepdive/cards/Main.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.cards;
+
+import edu.cnm.deepdive.cards.model.Deck;
+import java.util.random.RandomGenerator;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Deck deck = new Deck();
+    System.out.println(deck + "\n");
+
+    System.out.println(deck.deal() + "\n");
+
+    deck.shuffle(RandomGenerator.getDefault());
+    System.out.println(deck + "\n");
+
+    deck.sort(null);
+    System.out.println(deck);
+  }
+}
